eb9c9cb4200456fe0764f486e2cdc1ff457c8e10
canasmh <canasmh@nmsu.edu>
2021-01-12 16:55:46 -0500

    Added javascript so that the welcome page fades in nicely!

---
+++ index.js
@@ -0,0 +1,29 @@
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.welcome-header');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+$('.welcome-paragraph').hide();
+$('img.main-image').hide();
+
+anime.timeline()
+  .add({
+    targets: '.welcome-header .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 1200,
+    delay: (el, i) => 100 * (i+1)
+  });
+
+setTimeout(function(){
+    $('.trans--grow').addClass('main-hr');
+}, 100);
+
+setTimeout(function() {
+    $('.welcome-paragraph').fadeToggle(2000);
+}, 1900);
+
+setTimeout(function() {
+    $('img.main-image').fadeToggle(2000);
+}, 1900);
+
+
