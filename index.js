14a772b33b9f8df20cef6923f465f7d86c6f54be
canasmh <canasmh@nmsu.edu>
2021-01-16 15:42:41 -0500

    Collapse navbar if user clicks on a link -- Added media queries for "back to top" button

--- index.js
+++ index.js
@@ -29,6 +29,12 @@ setTimeout(function() {
 }, 3800);
 
 $("a.back-to-top").click(function() {
-  $("html, body").animate({ scrollTop: 0 }, 500);
+  $("html, body").animate({ scrollTop: 0 }, {
+      duration: 1000});
 });
+
+$("li.nav-item").click(function() {
+    $("div.navbar-collapse").removeClass("show");
+});
+
 
