45ed2379d1662586ce2d3e36156756b8770831ec
canasmh <canasmh@yahoo.com>
2022-09-08 23:16:31 -0400

    Move the new web-design pages to parent directory and delete the old files

--- index2.js
+++
@@ -1,8 +0,0 @@
-$("a.back-to-top").click(function() {
-  $("html, body").animate({ scrollTop: 0 }, {
-      duration: 1000});
-});
-
-$("li.nav-item").click(function() {
-    $("div.navbar-collapse").removeClass("show");
-});
